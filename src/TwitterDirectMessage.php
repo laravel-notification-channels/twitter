d4a3891d656fa457c5993805b81359a359d54d3f
Christoph Rumpel <c.rumpel@kabsi.at>
2016-10-30 16:16:53 +0100

    Remove type hinting

--- src/TwitterDirectMessage.php
+++ src/TwitterDirectMessage.php
@@ -20,7 +20,7 @@ class TwitterDirectMessage
     /*
      * @param  string $content
      */
-    public function __construct($to, string $content)
+    public function __construct($to, $content)
     {
         $this->to = $to;
         $this->content = $content;
